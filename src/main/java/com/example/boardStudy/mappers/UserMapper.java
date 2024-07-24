ab05c330ba727eccf0b8625ec3445ffe558a9d85
bjw5035 <send2ugfd@naver.com>
2024-07-24 23:49:39 +0900

    feat : 자바버전 수정

--- src/main/java/com/example/boardStudy/mappers/UserMapper.java
+++ src/main/java/com/example/boardStudy/mappers/UserMapper.java
@@ -1,10 +1,11 @@
 package com.example.boardStudy.mappers;
 
 import com.example.boardStudy.vo.UserVO;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Repository
+@Mapper
 public interface UserMapper {
 
     String memberLogin(@RequestParam UserVO userVO) throws Exception;
